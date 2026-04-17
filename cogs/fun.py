94a1324d1767a5978a77cf39b73005525e6b4661
Tyrell <rivax537@gmail.com>
2026-04-17 20:55:42 +0200

    Add gaydar command

--- cogs/fun.py
+++ cogs/fun.py
@@ -20,5 +20,31 @@ class Fun(commands.Cog):
         embed = discord.Embed(title=f"Pride Flag: {chosen[0]}", description=f"**Meaning:** {chosen[1]}", color=discord.Color.random())
         await interaction.response.send_message(embed=embed)
 
+    @app_commands.command(name="gaydar", description="A playful gaydar to test your friends!")
+    async def gaydar(self, interaction: discord.Interaction, user: discord.Member = None):
+        import random
+        user = user or interaction.user
+        percentage = random.randint(0, 100)
+        
+        if percentage <= 20:
+            remark = "Straight as a ruler! 📏 (يا شارع)"
+        elif percentage <= 40:
+            remark = "A little bit fruity... 🍓 (يامي)"
+        elif percentage <= 60:
+            remark = "Halfway out of the closet! 🚪"
+        elif percentage <= 80:
+            remark = "Pretty gay, we love to see it! 💅"
+        else:
+            remark = "Maximum gayness achieved! 🌈✨ (شواذ بلدنا)"
+
+        embed = discord.Embed(
+            title="Gaydar Result 🏳️‍🌈",
+            description=f"## {user.mention} is **{percentage}%** gay!\n\n-# *{remark}*",
+            color=discord.Color.random()
+        )
+        embed.set_thumbnail(url=user.display_avatar.url)
+        
+        await interaction.response.send_message(embed=embed)
+
 async def setup(bot):
     await bot.add_cog(Fun(bot))
